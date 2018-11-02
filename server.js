32519d15a99b063423cb58b08a510532accf2cbb
ThePuffin <ballutvincent@gmail.com>
2018-11-02 14:34:17 +0100

    add front in react

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 let app = express();
 
+const axios = require("axios");
+
 app.get("/", (req, res) => {
   res.send("<p>hello world</p>");
 });
+
+
+app.listen(3000);
