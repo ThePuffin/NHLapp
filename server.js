847c28eedc9ec9bbc2f2beb9189e3034ad36360d
ThePuffin <ballutvincent@gmail.com>
2018-11-03 19:53:53 +0100

    add json file

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 let app = express();
 
+const teams = require("./teams.json");
+
 const axios = require("axios");
 
 app.get("/", (req, res) => {
-  res.send("<p>hello world</p>");
+  res.send("<p>hello</p>");
 });
 
+app.get("/api", (req, res) => {
+  res.json(teams);
+});
 
 app.listen(3000);
