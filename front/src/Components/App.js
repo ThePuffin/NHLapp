081b9b3cd9eb73b970a98850f2e970fa9c8ea693
ThePuffin <ballutvincent@gmail.com>
2018-11-04 11:28:49 +0100

    add a new component for front

--- front/src/Components/App.js
+++ front/src/Components/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './images/logo.png';
-import './App.css';
+import React, { Component } from "react";
+import logo from "../images/logo.png";
+import "./App.css";
+import Register from "./register/Register";
 
 class App extends Component {
   render() {
@@ -8,10 +9,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to my NHL's stats.
-          </p>
+          <p>Welcome to my NHL's stats.</p>
         </header>
+        <Register />
       </div>
     );
   }
---
+++ front/src/Components/register/Register.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h2>Sign in</h2>
+        </header>
+        <main>
+          <label>Nom</label>
+          <input />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Register;
