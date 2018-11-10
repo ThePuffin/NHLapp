99094b7a02974c0da28a73b4876a8756bd6d60a6
ThePuffin <ballutvincent@gmail.com>
2018-11-10 11:51:11 +0100

    add a background and make register file

--- front/src/Components/App.js
+++ front/src/Components/App.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import logo from "../images/logo.png";
 import "./App.css";
 import Register from "./register/Register";
+import SearchAppBar from "./appBar/AppBar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SearchAppBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Welcome to my NHL's stats.</p>
+          <h1>Welcome to my NHL's stats.</h1>
         </header>
         <Register />
       </div>
--- front/src/Components/register/Register.js
+++ front/src/Components/register/Register.js
@@ -1,19 +1,137 @@
-import React, { Component } from "react";
+import React from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormControl from "@material-ui/core/FormControl";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 
-class Register extends Component {
+const styles = theme => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  margin: {
+    margin: theme.spacing.unit
+  },
+  withoutLabel: {
+    marginTop: theme.spacing.unit * 3
+  },
+  textField: {
+    flexBasis: 200
+  }
+});
+
+const ranges = [
+  {
+    value: "1",
+    label: "New Jersey Devils"
+  },
+  {
+    value: "2",
+    label: "New York Islanders"
+  },
+  {
+    value: "3",
+    label: "New York Rangers"
+  }
+];
+
+class InputAdornments extends React.Component {
+  state = {
+    nickname: "",
+    password: "",
+    team: "",
+    showPassword: false
+  };
+
+  handleChange = prop => event => {
+    this.setState({ [prop]: event.target.value });
+  };
+
+  handleClickShowPassword = () => {
+    this.setState(state => ({ showPassword: !state.showPassword }));
+  };
+
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        <header>
-          <h2>Sign in</h2>
-        </header>
-        <main>
-          <label>Nom</label>
-          <input />
-        </main>
+      <div className={classes.root}>
+        <TextField
+          label="Nickname"
+          id="simple-start-adornment"
+          value={this.state.nickname}
+          className={classNames(classes.margin, classes.textField)}
+        />
+        <TextField
+          select
+          label="Favorite Team"
+          className={classNames(classes.margin, classes.textField)}
+          value={this.state.team}
+          onChange={this.handleChange("team")}
+        >
+          {ranges.map(option => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <FormControl className={classNames(classes.margin, classes.textField)}>
+          <InputLabel htmlFor="adornment-password">Password</InputLabel>
+          <Input
+            id="adornment-password"
+            type={this.state.showPassword ? "text" : "password"}
+            value={this.state.password}
+            onChange={this.handleChange("password")}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Toggle password visibility"
+                  onClick={this.handleClickShowPassword}
+                >
+                  {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+
+        <FormControl className={classNames(classes.margin, classes.textField)}>
+          <InputLabel htmlFor="adornment-password">
+            Verify your Password
+          </InputLabel>
+          <Input
+            id="adornment-password2"
+            type={this.state.showPassword ? "text" : "password"}
+            value={this.state.verifyPassword}
+            onChange={this.handleChange("password")}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="Toggle password visibility"
+                  onClick={this.handleClickShowPassword}
+                >
+                  {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
       </div>
     );
   }
 }
 
-export default Register;
+InputAdornments.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(InputAdornments);
